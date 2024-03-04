5aa3b3321233fcd238c862c75b217912b8ece33a
Alo Angeli <aloangeli@gmail.com>
2024-03-05 00:43:00 +0100

    Update models.py

--- poly/vocab/models.py
+++ poly/vocab/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
 
 ## Traslator
 class Language(models.Model):
-    name = models.CharField(max_legth = 50)
+    name = models.CharField(max_length = 50)
     code = models.CharField(max_length = 10)
     
     def __str__(self) -> str:
@@ -14,43 +14,39 @@ class Language(models.Model):
     
 
 class Word(models.Model):
-    word = models.CharField(max_legth = 50)
+    word = models.CharField(max_length = 50)
     context = models.CharField(max_length = 300)
-    language = models.ForeignKey(Language)
+    language = models.ForeignKey(Language, on_delete = models.DO_NOTHING)
     
     def __str__(self) -> str:
         return self.word
     
-class WordClasses(models.enums):
-    NOUN          = 'Noun'
-    PRONOUN       = 'Pronoun'
-    VERB          = 'Verb'
-    ADJECTIVE     = 'Adjective'
-    ADVERB        = 'Adverb'
-    PREPOSITION   = 'Preposition'
-    CONJUNCTION   = 'Conjunction'
-    INTERJECTION  = 'Interjection'
-    DETERMINER    = 'Determiner'
-    ARTICLE       = 'Article'
-    
-    @classmethod
-    def choices(cls):
-        return [(key.value, key.name) for key in cls]
+class WordClasses(models.TextChoices):
+    NOUN = 'Noun', 'Noun'
+    PRONOUN = 'Pronoun', 'Pronoun'
+    VERB = 'Verb', 'Verb'
+    ADJECTIVE = 'Adjective', 'Adjective'
+    ADVERB = 'Adverb', 'Adverb'
+    PREPOSITION = 'Preposition', 'Preposition'
+    CONJUNCTION = 'Conjunction', 'Conjunction'
+    INTERJECTION = 'Interjection', 'Interjection'
+    DETERMINER = 'Determiner', 'Determiner'
+    ARTICLE = 'Article', 'Article'
     
 class Translation(models.Model):
-    target_language = models.ForeignKey(Language)
+    target_language = models.ForeignKey(Language, on_delete = models.DO_NOTHING)
     
     # Basic
-    translated_word = models.CharField(max_legth = 50)
-    translated_context = models.CharField(max_legth = 300)
+    translated_word = models.CharField(max_length = 50)
+    translated_context = models.CharField(max_length = 300)
     
     # Meta
     label = models.CharField(
         max_length=50,
-        choices=WordClasses.choices(),
+        choices=WordClasses.choices,
     )
 
 class Definition(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    target_language = models.ForeignKey(Language)
+    target_language = models.ForeignKey(Language, on_delete=models.CASCADE)
     definition_text = models.TextField()
