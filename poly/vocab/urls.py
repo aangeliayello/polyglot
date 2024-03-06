079fd034bab933739ba4ad38223772dfa6970eac
aangeliayello <aloangeli@gmail.com>
2024-03-06 13:00:30 +0100

    Add endpoint to save translations locally

--- poly/vocab/urls.py
+++ poly/vocab/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('word_with_context/', views.word_with_context_view, name='word_with_context'),
-    
+    path('word_with_context/', views.word_with_context_view, name='word_with_context_form'),
+    path("save_translation/", views.save_translation_result, name="save_translation_result"),
 ]
--- poly/vocab/views.py
+++ poly/vocab/views.py
@@ -39,4 +39,41 @@ def word_with_context_view(request):
     return render(request, 'vocab/word_context.html', {'form': form})
 
 
+def save_translation_result(request):
+    if request.method == 'POST':
+        # Extracting the translation data from the POST request
+        word = request.POST.get('word')
+        context = request.POST.get('context')
+        translated_word = request.POST.get('translated_word')
+        translated_context = request.POST.get('translated_context')
+        word_class = request.POST.get('word_class')
+        
+        # Building a dictionary to hold the data
+        new_translation_data = {
+            'word': word,
+            'context': context,
+            'translated_word': translated_word,
+            'translated_context': translated_context,
+            'word_class': word_class
+        }
     
+        # Define the filename (you might want to use a more unique or dynamic naming scheme)
+        filename = 'vocab/data/translation_data.json'
+        
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = {'translations': []}
+        
+        # Append the new translation to the translations list
+        data['translations'].append(new_translation_data)
+        
+        # Write the updated data back to the file with pretty formatting
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+        # Redirecting back to the form page after saving
+        return redirect('word_with_context_form')
+    else:
+        # If the request method is not POST, redirect to the form as well
+        return redirect('word_with_context_form')
